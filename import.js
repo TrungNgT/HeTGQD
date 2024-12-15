4c160189e9eedd57d792b7fea8a8617d012ffe37
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2024-12-16 03:30:02 +0700

    done index Data to elastic

--- import.js
+++ import.js
@@ -1,6 +1,8 @@
 import { readFile } from 'fs';
-import { join } from 'path';
+// import { join } from 'path';
 import { Client } from '@elastic/elasticsearch';
+// import { keyboard } from '@testing-library/user-event/dist/keyboard';
+// import { text } from 'stream/consumers';
 
 // Khởi tạo client Elasticsearch
 const client = new Client({
@@ -11,10 +13,59 @@ const client = new Client({
   }
 });
 
+// create the index 'de_tai' in the defined cluster.
+await client.indices.create({
+  index: "de_tai",
+  settings: {
+    number_of_shards: 1,
+    analysis: {
+      analyzer: {
+        my_analyzer: {
+          type: "standard",
+          filter: "lowercase"
+        }
+      }
+    }
+  },
+  mappings: {
+    properties: {
+      "id": {
+        type: "keyword"
+      },
+      "Tên giảng viên": {
+        type: "text",
+        analyzer: "my_analyzer",
+        search_analyzer: "my_analyzer"
+      }, 
+      "Khoa": {
+        type: "text",
+        analyzer: "my_analyzer",
+        search_analyzer: "my_analyzer"
+      }, 
+      "Tên đề tài": {
+        type: "text",
+        analyzer: "my_analyzer",
+        search_analyzer: "my_analyzer"
+      },
+      "Hệ": {
+        type: "text",
+        analyzer: "my_analyzer",
+      },
+      "Chi tiết": {
+        type: "text",
+        analyzer: "my_analyzer",
+        search_analyzer: "my_analyzer"
+      }
+    }
+  }
+})
 
+client.info().then(console.log, console.log)
 
 // Đọc dữ liệu từ file dataDoAn.json trong thư mục public
-const filePath = join(__dirname, 'public', 'dataDoAn.json');
+//const filePath = join(__dirname, 'public', 'dataDoAn.json');
+
+const filePath = "./public/dataDoAn.json"
 
 readFile(filePath, 'utf8', async (err, data) => {
   if (err) {
